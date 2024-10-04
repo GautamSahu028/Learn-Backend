ce9532a850b87f73ddfc8017b85fb98aa6ee17b8
Gautam <gautamsahu19102002@gmail.com>
2024-10-04 14:36:53 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -4,26 +4,18 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 // DATA  is received by backend from many sources : url, json, from body...
 // we can use express.json() to parse json data from body
-app.use(
-  express.json({
-    limit: "16kb",
-  })
-);
+app.use(express.json({ limit: "16kb" }));
 // t0 handle data coming from URL
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  })
-);
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 export { app };
